e1b2582202f5598f62ebcf232d21a60265900c4e
Nabe <vohongduc000@gmail.com>
2024-12-02 15:01:59 +0700

    add sidebar chat

---
+++ src/components/Cat.js
@@ -0,0 +1,9 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+
+const Cat = () => {
+  return (
+    <Avatar src="https://www.reddit.com/r/PixelArt/comments/15z47dt/i_got_bored_so_i_decided_to_draw_a_random_image/" />
+  );
+};
+export default Cat;
